b5dcf6f808b0de2527b57a2c0170f9218062d81a
Katya Weicht <katya.weicht@gmail.com>
2025-07-08 21:17:56 -0600

    Set up initial class attributes

--- lib/song.py
+++ lib/song.py
@@ -1,7 +1,12 @@
 class Song:
-    song_count = 0
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
     def __init__(self,name,artist,genre):
         self.name = name
         self.artist = artist
         self.genre = genre
-        Song.song_count += 1
+        Song.count += 1
